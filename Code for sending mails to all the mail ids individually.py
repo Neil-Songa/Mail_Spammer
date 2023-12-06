1b5bd6f6a8682fd9d61fe955673cb22e8108877b
Neil-Songa <121611129+Neil-Songa@users.noreply.github.com>
2023-12-06 12:27:51 +0530

    Add files via upload

---
+++ Code for sending mails to all the mail ids individually.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import smtplib
+
+df = pd.read_excel('Trail.xlsx')
+
+# Replace 'email' with the column name containing the email addresses
+# Replace 'subject' with the subject of the email
+# Replace 'body' with the body of the email
+for index, row in df.iterrows():
+    email = row['Email']
+    gender = row['P']
+    subject = 'Request for Internship Opportunity'
+    body1 = '''
+Dear Sir,
+
+I hope this email reaches you well. I am sending this email to ask about an internship opportunity for this summer. As a student of IIT KHARAGPUR, I would like to gain practical experience in the Data Analytics field. I think this is the perfect platform to learn and grow. I am particularly interested in Data Analytics and am impressed by the company's commitment to the usage of Analytics and Tech Consolidation.
+
+I am excited about the possibility of contributing to your team while also developing my skills and knowledge. 
+
+Here is the link for my CV
+
+Thank you for your time and consideration. I look forward to hearing from you.
+Sincerely, 
+
+Neil Songa
+'''
+    body2 = '''
+Dear Madam,
+
+I hope this email reaches you well. I am sending this email to ask about an internship opportunity for this summer. As a student of IIT KHARAGPUR, I would like to gain practical experience in the Data Analytics field. I think this is the perfect platform to learn and grow. I am particularly interested in Data Analytics and am impressed by the company's commitment to the usage of Analytics and Tech Consolidation.
+
+I am excited about the possibility of contributing to your team while also developing my skills and knowledge. 
+
+Here is the link for my CV
+
+Thank you for your time and consideration. I look forward to hearing from you.
+Sincerely, 
+
+Neil Songa
+'''
+    if gender == 'Mr.':
+      body=body1
+    else:
+      body=body2
+    # Enter your email and password below
+    # You will get the sender password in your account security>>app passwords
+    #if you are unable to find app passwords the complete the 2 step verification in security
+    sender_email = 'neilpratap2016@gmail.com'
+    sender_password = 'cipl isxr yrug pxvk'
+    
+    # Set up the SMTP server
+    smtp_server = 'smtp.gmail.com'
+    port = 587
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls()
+    server.login(sender_email, sender_password)
+    
+    # Create the email message
+    message = f'Subject: {subject}\n\n{body}'
+    
+    # Send the email
+    server.sendmail(sender_email, email, message)
+    
+    # Close the SMTP server
+    server.quit()
